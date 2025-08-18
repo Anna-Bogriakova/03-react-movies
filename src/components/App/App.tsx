19661d56b242318cc96d18fa110ce6b2e65242ff
Anna Bogriakova <anna.bogriakova@gmail.com>
2025-08-18 23:38:03 +0200

    changed app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,7 +7,7 @@ import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
 
-import { fetchMovies } from "../../services/MovieService";
+import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
 import "./App.module.css";
 
