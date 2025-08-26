e86d8bf6f0ba7b42b5d314b101e5a4c16911bd0d
Anna Bogriakova <anna.bogriakova@gmail.com>
2025-08-26 21:42:04 +0200

    updated movieService

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,6 +3,7 @@ import type { Movie } from "../types/movie";
 
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+console.log("TOKEN:", TOKEN);
 
 interface FetchMoviesResponse {
   results: Movie[];
